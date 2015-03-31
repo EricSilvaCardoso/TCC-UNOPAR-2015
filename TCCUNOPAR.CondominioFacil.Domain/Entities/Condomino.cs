bc2ff57d1fccb8ba2ec08cd9fa9c566da67a5f90
EricSilvaCardoso <eric_674@msn.com>
2015-03-31 19:38:54 -0300

    Condomino

---
+++ TCCUNOPAR.CondominioFacil.Domain/Entities/Condomino.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TCCUNOPAR.CondominioFacil.Domain.Entities
+{
+    public class Condomino
+    {
+        public Condomino()
+        {
+            CondominoId = Guid.NewGuid();
+        }
+
+
+        public Guid CondominoId { get; set; }
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public string Email { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public bool Ativo { get; set; }
+        //public virtual ICollection<Telefone> TelefoneList { get; set; }
+
+
+    }
+}
---
+++ TCCUNOPAR.CondominioFacil.Domain/Entities/CondominoDependente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TCCUNOPAR.CondominioFacil.Domain.Entities
+{
+    public class CondominoDependente
+    {
+        public CondominoDependente()
+        {
+            CondominoDependenteId = Guid.NewGuid();
+        }
+
+        public Guid CondominoDependenteId { get; set; }
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public string Email { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public bool Ativo { get; set; }
+
+
+        //public virtual ICollection<Telefone> TelefoneList{ get; set; }
+    }
+}
