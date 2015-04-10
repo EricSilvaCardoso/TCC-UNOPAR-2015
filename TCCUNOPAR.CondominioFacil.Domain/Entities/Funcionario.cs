c7c69226a2664135259b33f95e6b7e76ea3d7533
EricSilvaCardoso <eric_674@msn.com>
2015-04-10 15:19:24 -0300

    EntidadesInterfaces

---
+++ TCCUNOPAR.CondominioFacil.Domain/Entities/Funcionario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCCUNOPAR.CondominioFacil.Domain.Entities
+{
+    public class Funcionario
+    {
+        public Funcionario()
+        {
+            FuncionarioID = Guid.NewGuid();
+        }
+
+        public Guid FuncionarioID { get; set; }
+        public string Nome { get; set; }
+        public int Sexo { get; set; }
+        public string Apelido { get; set; }
+        public string Endereco { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public bool Ativo { get; set; }
+
+        public virtual IEnumerable<TelefoneAss> TelefoneList { get; set; }
+
+
+    }
+}
